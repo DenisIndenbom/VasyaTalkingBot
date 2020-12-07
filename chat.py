9d7eb2811630918036d757cde9b6aaf8321cc949
DenisIndenbom <59681620+DenisIndenbom@users.noreply.github.com>
2020-12-07 13:00:42 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -2,8 +2,11 @@ import telebot
 from telebot.types import Message, User
 import dialogModel as rnn
 import datetime
-#fitdate = datetime.datetime.now().hour + 1
-model = rnn.Model( 'idx2char.npy', 'ckpt_30' )
+import keepAwake
+keepAwake.enable()
+
+
+model = rnn.Model( 'idx2char.npy', 'ckpt_35' )
 token = "secret"
 def writeMessage(user,question,answer):
     writeLog(f'{user}: {question}\nВася: {answer}')
@@ -15,14 +18,14 @@ def writeLog(text):
 
     print(text)
 # Обходим блокировку с помощью прокси
-telebot.apihelper.proxy = {'https': 'proxy'}
+telebot.apihelper.proxy = {'https': 'socks5h://geek:socks@t.geekclass.ru:7777'}
 # подключаемся к телеграму
 bot = telebot.TeleBot(token=token)
 
 @bot.message_handler(commands=['github'])
 def send_github(message: Message):
     bot.send_message(message.chat.id,'Ссылка на GitHub: https://github.com/DenisIndenbom/VasyaTalkingBot')
-@bot.message_handler(commands=['creator'])
+@bot.message_handler(commands=['creator'])  
 def send_creator(message: Message):
     bot.send_message(message.chat.id,'Меня создал Денис Инденбом.Все исходники на GitHub /github')
 
@@ -32,22 +35,39 @@ def send_help(message: Message):
                                      '\nЯ буду отвечать тебе на сообщение (просто напиши мне сообщение).'
                                      '\nВнимание бот может отвечать не совсем коректно!'
                                      '\nКоманды:'
-                                     '\n/description'
-                                     '\n/creator'
-                                     '\n/github')
-
+                                     '\n/description - описание'
+                                     '\n/ver - версия бота и размер его датасета'
+                                     '\n/news - новости последнего обновления'
+                                     '\n/creator - создатель'
+                                     '\n/github - исходники')
+@bot.message_handler(commands=['ver'])
+def ver(message: Message):
+    bot.send_message( message.chat.id,"Версия бота 1.6;\nРазмер датасета 330573 сообщений;\nКол-во параметров нейронной сети 6,520,290;")
+@bot.message_handler(commands=['news'])
+def ver(message: Message):
+    bot.send_message( message.chat.id,"Обновление 1.6!\n"
+                                      "\nЧто нового:"
+                                      "\n - датасет увеличен в 6 раз => Вася отвечает корректнее \n   и осмысленнее.(Датасет - это начём учится нейроная сеть)"
+                                      "\n"
+                                      "\nВывод:"
+                                      "\n В этом обновление Вася отвечает корректнее и осмысленнее."
+                                      "\n То-есть лучше составляет предложения по контексту вопроса.")
 @bot.message_handler(commands=['description'])
 def send_description(message: Message):
     bot.send_message(message.chat.id,'Вася - говорящий бот.'
-                                     '\nОн сможет поболтать с вами о чём угодно, но может отвечать некорректно.'
+                                     '\n'
+                                     '\nОн может поболтать с вами о чём угодно, но может отвечать некорректно.'
+                                     '\n'
+                                     '\nБот использует RNN(рекуррентная нейронная сеть) для генерации текста.'
+                                     '\n'
                                      '\nНа данный момент бот обучается говорить лучше.'
-                                     '\nМожет через некоторое время он станет намного лучше чем сейчас.')
+                                     'Может через некоторое время он станет намного лучше чем сейчас.')
 
 @bot.message_handler(commands=['start'])
 def start(message: Message):
     user: User = message.from_user
     bot.send_message(message.chat.id,
-                     'Привет!\U0001F91A. Я чат бот \U0001F916 и я могу с тобой поболтать.'
+                     'Привет!\U0001F91A. Я говорящий чат бот \U0001F916. Я использую нейронные сети для генерации текста'
                      '\nПомощь:/help'
                      '\nСсылка на gitHub: https://github.com/DenisIndenbom/VasyaTalkingBot')
     writeLog(f"{user.first_name}: {message.text}")
@@ -57,28 +77,31 @@ def talk(message: Message):
     answer = ""
     global model
     user: User = message.from_user
-    text = message.text
-    try:
-        answer = model.generate_text( f'< {text}\n> ', True, 0.2)
-        answer = answer.rstrip( '\n\r' )
-        writeMessage( user.first_name, text, answer )
-    except:
+    text = message.text.replace('-','')
+    if "main" in text:
+        bot.send_message( message.chat.id, 'Не понял' )
+    else:
         try:
-            bot.send_message( message.chat.id, 'В вашем сообщении не понятные для меня символы!' )
+            answer = model.generate_text( f'< {text}\n> ', True, 0.15)
+            answer = answer.rstrip( '\n\r' )
+            writeMessage( user.first_name, text, answer )
         except:
-                print( "error:message didn't send" )
+            try:
+                bot.send_message( message.chat.id, 'В вашем сообщении не понятные для меня символы!' )
+            except:
+                    print( "error:message didn't send" )
 
-    try:
-        bot.send_message( message.chat.id,answer)
-    except:
         try:
-            bot.send_message( message.chat.id, answer )
+            bot.send_message( message.chat.id,answer)
         except:
-            print("error:message didn't send")
-            while True:
-                try:
-                    bot.send_message( message.chat.id, answer )
-                    break
-                except:
-                    pass
-bot.polling(none_stop=True)
+            try:
+                bot.send_message( message.chat.id, answer )
+            except:
+                print("error:message didn't send")
+                while True:
+                    try:
+                        bot.send_message( message.chat.id, answer )
+                        break
+                    except:
+                        pass
+bot.polling(none_stop=True,timeout=100)
