2582c655fd7b743b4543ea3a2ec46f60054458e5
DenisIndenbom <59681620+DenisIndenbom@users.noreply.github.com>
2021-02-15 15:35:56 +0300

    Update dialogModel.py

--- dialogModel.py
+++ dialogModel.py
@@ -1,16 +1,20 @@
-import functools
-import tensorflow as tf
 import numpy as np
+print("import tensorflow")
+import tensorflow as tf
 
+print("loading rnn")
 embedding_dim = 256
 rnn_units = 1024
-rnn = functools.partial( tf.keras.layers.LSTM, recurrent_activation = 'sigmoid' )
+rnn = tf.compat.v1.keras.layers.LSTM
 
+dataPath = ''
 
 class Model:
     def __init__( self, vocabFileName: str, modelFileName: str ):
+        print('loading vocab')
         self.__idx2char = np.load( vocabFileName )
         self.__char2idx = { u: i for i, u in enumerate( self.__idx2char ) }
+        print("building model")
         self.__model = self.__buildModel( self.__idx2char.shape[0], 1 )
         self.__model.load_weights( modelFileName )
         self.__model.build( tf.TensorShape( [1, None] ) )
@@ -30,14 +34,14 @@ class Model:
 
     def generate_text( self, start_string, oneString, temperature ):
         # Максимальное количество генерируемых символов
-        num_generate = 200
+        num_generate = 150
         input_eval = [self.__char2idx[s] for s in start_string]
         input_eval = tf.expand_dims( input_eval, 0 )
 
         text_generated = []
 
         self.__model.reset_states()
-        for i in range( num_generate ):
+        for i in range(num_generate):
             predictions = self.__model( input_eval )
             predictions = tf.squeeze( predictions, 0 )
 
@@ -53,9 +57,3 @@ class Model:
                 break
 
         return  ''.join( text_generated )
-
-
-if __name__ == '__main__':
-    model = Model( 'idx2char.npy', 'ckpt_30' )
-    while True:
-        print( model.generate_text( f'< {input()}\n> ', True, 0.15) )
